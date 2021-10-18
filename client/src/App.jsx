188dc6d325793b75c2d1134172f1cf3604abbafd
Rohit Patil <patilrohit.official@gmail.com>
2021-10-18 13:17:30 +0530

    Frontend work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer,ChannelContainer } from './components';
 
+import './App.css';
+
 const apiKey = 'qd2654cgn9fz';
 
 const client = StreamChat.getInstance(apiKey);
