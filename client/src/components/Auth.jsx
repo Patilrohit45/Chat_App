9d1dceab0778edacae95d2daa58f4ac8156667d6
Rohit Patil <patilrohit.official@gmail.com>
2021-10-18 20:32:04 +0530

    server side Installation

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,10 +4,29 @@ import axios from 'axios';
 
 import siginImage from '../assets/signup.jpg';
 
+const initialState = {
+    fullName: '',
+    username:'',
+    password:'',
+    confirmPassword:'',
+    phoneNumber:'',
+    avatarURL:'',
+}
+
 const Auth = () => {
+    const [form, setForm] = useState(initialState);
     const [issignup, setIsSignup] = useState(true);
 
-    const handleChange = ()=>{}
+    const handleChange = (e)=>{
+        setForm({...form, [e.target.name]: e.target.value});
+        
+        
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log(form);
+    }
 
     const switchMode = () => {
         setIsSignup((prevIsSignup)=> !prevIsSignup)
@@ -18,7 +37,7 @@ const Auth = () => {
             <div className="auth__form-container_fields">
                     <div className="auth__form-container_fields-content">
                     <p>{issignup ? 'Sign Up' : 'Sign In'}</p>
-                    <form onSubmit={()=>{}}>
+                    <form onSubmit={handleSubmit}>
                     {issignup && (
                         <div className="auth__form-container_fields-content_input">
                             <label htmlFor="fullName">Full Name</label>
@@ -98,6 +117,9 @@ const Auth = () => {
                         
                         </div>
                             )}
+                            <div className="auth__form-container_fields-content_button">
+                                <button>{issignup ? "Sign Up": "Sign In"}</button>
+                            </div>
                     </form>
                     <div className="auth__form-container_fields-account">
                         <p>
